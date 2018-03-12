46ea9e7606ff3906f9810cdcf2ad7fedd3eecbfa
Bao Huynh <huynhminhbao2808@gmail.com>
2018-03-12 21:27:15 +0700

    create scene game

---
+++ BardTaleLegendof AsceronGame/Assets/Script/EnterGameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterGameManager : MonoBehaviour {
+    public void LoadMapScene () {
+        SceneManager.LoadScene(1);
+    }
+}
