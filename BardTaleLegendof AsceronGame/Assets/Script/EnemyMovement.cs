fd7bb6a2ce87d57b9408becadc7083fe04a2f950
Bao Huynh <huynhminhbao2808@gmail.com>
2018-07-31 09:50:22 +0700

    add more raycast for enemy

--- BardTaleLegendof AsceronGame/Assets/Script/EnemyMovement.cs
+++ BardTaleLegendof AsceronGame/Assets/Script/EnemyMovement.cs
@@ -8,7 +8,7 @@ public class EnemyMovement : MonoBehaviour {
 	[SerializeField] private float rayLength;
 	[SerializeField] private LayerMask layer;
 
-	private bool isHit;
+	private bool isHitForward, isHitRight45Degree, isHitLeft45Degree;
 	private GameObject player;
 	private bool enemySeePlayer = false;
 	private Vector2 enemyStartPosition;
@@ -29,15 +29,18 @@ public class EnemyMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var target = player.transform.position;
-		isHit = Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),rayLength,layer.value);
+		isHitForward = Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),rayLength,layer.value);
+		isHitRight45Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward + Vector3.right).normalized), rayLength, layer.value);
+		isHitLeft45Degree = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward + Vector3.left).normalized, rayLength, layer.value);
+		Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward + Vector3.right).normalized) * rayLength, Color.yellow);
+		Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward + Vector3.left).normalized) * rayLength, Color.yellow);
 		Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayLength, Color.yellow);
-		if (isHit) {
+		if (isHitForward || isHitRight45Degree || isHitLeft45Degree) {
 			Debug.Log("a");
 			//DataManager.instance.listEnemyDefeatedPosition.Enqueue(enemyStartPosition);
 			//Destroy(this.gameObject);
 			//GameManager.instance.GoToBattle();
 			enemySeePlayer = true;
-
 		}
 		if (enemySeePlayer == true) {
 			//move to player
