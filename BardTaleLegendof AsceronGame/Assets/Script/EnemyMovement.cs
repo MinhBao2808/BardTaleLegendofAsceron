1cbedf5405b67d536565c7c2d7e839c92691f5b7
Bao Huynh <huynhminhbao2808@gmail.com>
2018-08-05 20:07:12 +0700

    make checkpoint for enemy

--- BardTaleLegendof AsceronGame/Assets/Script/EnemyMovement.cs
+++ BardTaleLegendof AsceronGame/Assets/Script/EnemyMovement.cs
@@ -6,64 +6,97 @@ public class EnemyMovement : MovingObject {
 	[SerializeField] private float rayLength;
 	[SerializeField] private float timeToFollowPlayer;
 	[SerializeField] private LayerMask layer;
-
-	private bool isHitForward, isHitRight45Degree, isHitLeft45Degree, isHitRight30Degree, isHitLeft30Degree, isHitRight60Degree, isHitLeft60Degree, isHitRight15Degree, isHitLeft15Degree;
+	[SerializeField] private Transform[] checkPoint;
+	private int targetPoint = 0;
+	private float currentTime;
+	//private bool isHitForward, isHitRight45Degree, isHitLeft45Degree, isHitRight30Degree, isHitLeft30Degree, isHitRight60Degree, isHitLeft60Degree, isHitRight15Degree, isHitLeft15Degree;
 	private GameObject player;
 	private bool enemySeePlayer = false;
-	private Vector2 enemyStartPosition;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag("Player").gameObject;
-		enemyStartPosition = this.transform.position;
-		foreach (Vector2 enemy in DataManager.instance.listEnemyDefeatedPosition) {
-			if(enemyStartPosition == enemy) {
-				Destroy(this.gameObject);
-			}
-		}
+		//foreach (Vector2 enemy in DataManager.instance.listEnemyDefeatedPosition) {
+		//	if(enemyStartPosition == enemy) {
+		//		Destroy(this.gameObject);
+		//	}
+		//}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//var a = yourAngleInDegree * Mathf.Deg2Rad;
 		var target = player.transform.position;
-		isHitForward = Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),rayLength,layer.value);
-		isHitRight45Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward + Vector3.right).normalized), rayLength, layer.value);
-		isHitLeft45Degree = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward + Vector3.left).normalized, rayLength, layer.value);
-		isHitRight15Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(15 * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(15 * Mathf.Deg2Rad)).normalized), rayLength, layer.value);
-		isHitLeft15Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(-15 * Mathf.Deg2Rad) + Vector3.left * Mathf.Sin(-15 * Mathf.Deg2Rad)).normalized), rayLength, layer.value);
-		//Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward + Vector3.right).normalized) * rayLength, Color.yellow);
-		//Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward + Vector3.left).normalized) * rayLength, Color.yellow);
-		//Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayLength, Color.red);
-		//Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(15 * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(15 * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
-		//Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(-15 * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(-15 * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
-		float angles = transform.eulerAngles.y - 60;
-		for (int i = 0; i < 7; i++) {
-			Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(i * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(i * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
-            angles += 15;
-        }
+		//isHitForward = Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),rayLength,layer.value);
+		//isHitRight45Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward + Vector3.right).normalized), rayLength, layer.value);
+		//isHitLeft45Degree = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward + Vector3.left).normalized, rayLength, layer.value);
+		//isHitRight15Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(15 * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(15 * Mathf.Deg2Rad)).normalized), rayLength, layer.value);
+		//isHitLeft15Degree = Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(-15 * Mathf.Deg2Rad) + Vector3.left * Mathf.Sin(-15 * Mathf.Deg2Rad)).normalized), rayLength, layer.value);
+		////Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward + Vector3.right).normalized) * rayLength, Color.yellow);
+		////Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward + Vector3.left).normalized) * rayLength, Color.yellow);
+		////Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayLength, Color.red);
+		////Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(15 * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(15 * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
+		////Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(-15 * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(-15 * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
+		//float angles = transform.eulerAngles.y - 120;
+		//for (int i = 0; i < 14; i++) {
+		//	if (i <= 7) {
+		//		Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(i * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(i * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
+		//	}
+		//	else {
+		//		Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(i * Mathf.Deg2Rad) + Vector3.left * Mathf.Sin(i * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
+		//	}
+		//	//Debug.DrawRay(transform.position, transform.TransformDirection((Vector3.forward * Mathf.Cos(i * Mathf.Deg2Rad) + Vector3.right * Mathf.Sin(i * Mathf.Deg2Rad)).normalized) * rayLength, Color.red);
+  //          angles += 7;
+  //      }
 
-		if (isHitForward || isHitRight45Degree || isHitLeft45Degree) { 
-			//DataManager.instance.listEnemyDefeatedPosition.Enqueue(enemyStartPosition);
-			//Destroy(this.gameObject);
-			//GameManager.instance.GoToBattle();
-			enemySeePlayer = true;
-		}
-		if (enemySeePlayer == true && timeToFollowPlayer >=0) {
+		//if (isHitForward || isHitRight45Degree || isHitLeft45Degree) { 
+		//	//DataManager.instance.listEnemyDefeatedPosition.Enqueue(enemyStartPosition);
+		//	//Destroy(this.gameObject);
+		//	//GameManager.instance.GoToBattle();
+		//	enemySeePlayer = true;
+		//}
+		if (enemySeePlayer == true && currentTime >=0) {
+			currentTime -= Time.deltaTime;
+			Debug.Log(currentTime);
 			//move to player
 			Vector3 lookAt = player.transform.position;
             lookAt.y = transform.position.y;
             transform.LookAt(lookAt);
 			transform.position = Vector3.MoveTowards(transform.position, target, sprintSpeed * Time.deltaTime);
 		}
+		else {
+			currentTime = timeToFollowPlayer;
+			enemySeePlayer = false;
+			Vector3 lookAt = checkPoint[targetPoint].transform.position;
+			lookAt.y = transform.position.y;
+			transform.LookAt(lookAt);
+			transform.position = Vector3.MoveTowards(transform.position, checkPoint[targetPoint].transform.position, walkSpeed * Time.deltaTime);
+		}
 	}
 
 	private void OnTriggerEnter(Collider collision) {
 		if (collision.gameObject.tag == "Player") {
-			//enemySeePlayer = true;
-			DataManager.instance.listEnemyDefeatedPosition.Enqueue(enemyStartPosition);
+			enemySeePlayer = true;
+			//DataManager.instance.listEnemyDefeatedPosition.Enqueue(enemyStartPosition);
+            //Destroy(this.gameObject);
+            //GameManager.instance.GoToBattle();
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
             Destroy(this.gameObject);
             GameManager.instance.GoToBattle();
+		}
+		if (collision.gameObject.tag == "CheckPoint") {
+			if (targetPoint == 1) {
+				targetPoint = 0;
+				Debug.Log("a");
+			}
+			else {
+				targetPoint += 1;
+				Debug.Log("b");
+			}
 		}
 	}
 }
--- BardTaleLegendof AsceronGame/Assets/Script/PlayerMovement.cs
+++ BardTaleLegendof AsceronGame/Assets/Script/PlayerMovement.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MovingObject {
     public static PlayerMovement instance = null;
-	[SerializeField] private GameObject StatPanel;
-	private Animator animator;
-	private Rigidbody rigid;
 	private CharacterController characterController;
-	[SerializeField] private Transform cameraT;
-	[SerializeField] private float inputDelay = 0.1f;
-	[SerializeField] private float forwardVel = 12;
-	[SerializeField] private float rotateVel = 100;
 	private Quaternion targetRotation;
-	private float forwardInput, turnInput; 
-	[SerializeField] private float speed;
-	[SerializeField] private float rotateSpeed = 3.0f;
 	[SerializeField] private float turnSmoothTime;
 	private float turnSmoothVelocity;
 	public float speedSmoothTime = 0.1f;
     float speedSmoothVelocity;
     float currentSpeed;
-    private GameManager gameManager = new GameManager();
-    
     
 	private void Awake() {
         if (instance == null) {
@@ -33,79 +21,29 @@ public class PlayerMovement : MovingObject {
 
 	private void Start() {
 		targetRotation = transform.rotation;
-		rigid = GetComponent<Rigidbody>();
-		animator = GetComponent<Animator>();
+		anim = GetComponent<Animator>();
 		characterController = GetComponent<CharacterController>();
-		forwardInput = turnInput = 0;
 	}
-
-	public Quaternion TargetRotation {
-		get {
-			return targetRotation;
-		}
-	}
-
-	void GetInput () {
-		forwardInput = Input.GetAxis("Vertical");
-		turnInput = Input.GetAxis("Horizontal");
-	}
+  
 
 	void Update() {
 		//GetInput();
 		//Turn();
-		if (Input.GetKeyDown(KeyCode.LeftShift)) {
-			currentSpeed = sprintSpeed;
-		}
-		else {
-			currentSpeed = walkSpeed;
-		}
+		isRun = Input.GetKey(KeyCode.LeftShift);
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		Vector2 inputDir = input.normalized;
 		if (inputDir != Vector2.zero) {
+			currentSpeed = ((isRun) ? sprintSpeed : walkSpeed) * inputDir.magnitude;
 			float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
 			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
-			animator.SetBool("Moving", true);
+			anim.SetBool("Moving", true);
 			Vector3 velocity = transform.forward * currentSpeed;
 			characterController.Move(velocity * Time.deltaTime);
 		}
 		else {
-			animator.SetBool("Moving", false);
+			anim.SetBool("Moving", false);
 		}
-
-		//currentSpeed = Mathf.SmoothDamp(currentSpeed, speed * inputDir.magnitude, ref speedSmoothVelocity, speedSmoothTime);
-
-        
-		//currentSpeed = new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
-		if (Input.GetKeyDown("e")) {
-			StatPanel.SetActive(true);
-        }
-
-		if (Input.GetKeyDown("escape")) {
-			StatPanel.SetActive(false);
-        }
 	}
-
-	//private void FixedUpdate() {
-	//	Run();
-	//}
-
-	//void Run() {
-	//	if (Mathf.Abs(forwardInput) > inputDelay) {
-	//		rigid.velocity = transform.forward * forwardInput * forwardVel;
-	//		animator.SetBool("Moving", true);
-	//	}
-	//	else {
-	//		rigid.velocity = Vector3.zero;
-	//		animator.SetBool("Moving", false);
-	//	}
-	//}
-
-	//void Turn() {
-	//	if (Mathf.Abs(turnInput) >inputDelay) {
-	//		targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
-	//	}
-	//	transform.rotation = targetRotation;
-	//}
 
 	public Vector2 ReturnPlayerPosition() {
         Vector2 playerPosition = transform.position;
