b0eb232ce3248aa4801bb425d044b41be1330510
Truong Dang <dangdzuytruong@gmail.com>
2018-08-22 01:30:09 +0700

    [Add] Create Data Class for Unit

---
+++ BardTaleLegendof AsceronGame/Assets/Script/Data/ItemStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStats : UnitBaseStats {
+    public enum Rarity
+    { Common, Uncommon, Rare, Mythical, Legendary, Relic}
+    public enum Type
+    { Equipment, Usable, Recipe, Miscellaneous, QuestItem}
+    public int ItemLevel { get; set; }
+    public int RequirementLevel { get; set; }
+    public Rarity ItemRarity { get; set; }
+    public Type ItemType { get; set; }
+}
---
+++ BardTaleLegendof AsceronGame/Assets/Script/Data/UnitBaseStats.cs
@@ -0,0 +1,15 @@
+﻿public class UnitBaseStats {
+    public float MaxHP { get; set; }
+    public float MaxMP { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Intelligence { get; set; }
+    public int Vitality { get; set; }
+    public int Endurance { get; set; }
+    public int Wisdom { get; set; }
+    public int FireRes { get; set; }
+    public int LightningRes { get; set; }
+    public int IceRes { get; set; }
+    public int HolyRes { get; set; }
+    public int DarkRes { get; set; }
+}
