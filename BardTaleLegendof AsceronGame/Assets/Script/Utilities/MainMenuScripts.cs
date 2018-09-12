837ac71b9e536431bec2c512bedd9543ba17495d
Truong Dang <dangdzuytruong@gmail.com>
2018-09-12 18:22:48 +0700

    [Update] Modify Main Menu - 12/09/2018

---
+++ BardTaleLegendof AsceronGame/Assets/Script/Utilities/MainMenuScripts.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ORKFramework;
+
+public class MainMenuScripts : MonoBehaviour {
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ BardTaleLegendof AsceronGame/Assets/Script/Utilities/OptionsScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsScripts : MonoBehaviour {
+    public AudioMixer audioMixer;
+
+    public void SetMasterVolume (float volume)
+    {
+        audioMixer.SetFloat("masterVol", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVol", volume);
+    }
+
+    public void SetVoiceVolume(float volume)
+    {
+        audioMixer.SetFloat("voiceVol", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("sfxVol", volume);
+    }
+}
